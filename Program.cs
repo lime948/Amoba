b5e27a2813dbfb6df9033bfb53cfb3fbf5837fa1
OresZeteny <144881224+lime948@users.noreply.github.com>
2025-09-25 12:14:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amoba
+{
+    internal class Program
+    {
+        static int games = 0;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("=== AMŐBA ===");
+            GameLoop();
+            Display();
+            Winconditions();
+            if (games >= 1)
+                Replay();
+            
+        }
+        static bool Replay()
+        {
+            bool ujra = false;
+            do
+            {
+                Console.WriteLine("Szeretnél újra játszani? (i/n)");
+                string valasz = Console.ReadLine().ToLower();
+                if (valasz == "i")
+                {
+                    Console.WriteLine("Új játék indul...");
+                    ujra = true;
+                    GameLoop();
+                    Display();
+                    Winconditions();
+                    break;
+                }
+                else if (valasz == "n")
+                {
+                    ujra = false;
+                    Console.WriteLine("Köszönöm, hogy játszottál!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hibás válasz, kérlek írd be újra!");
+                }
+            } while (ujra);
+            return ujra;
+        }
+
+        static void GameLoop()
+        {
+           
+        }
+
+        static void Display()
+        {
+
+        }
+
+        static void Winconditions()
+        {
+            //nyerés és vsztés után games += 1;
+        }
+    }
+}
