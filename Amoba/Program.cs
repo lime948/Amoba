23a653a5ceca7e24b47b1674ce5ad95cf699636a
OreskoZ <oreskoz@kkszki.hu>
2025-09-25 11:05:19 +0200

    1

--- Amoba/Program.cs
+++ Amoba/Program.cs
@@ -10,7 +10,37 @@ namespace Amoba
     {
         static void Main(string[] args)
         {
+        }
 
+        static void GameLoop()
+        {
+            Console.WriteLine("=== AMŐBA ===");
+            bool ujra = false;
+            do
+            {
+                Console.WriteLine("Szeretnél újra játszani? (i/n)");
+                string valasz = Console.ReadLine().ToLower();
+                if (valasz == "i")
+                {
+                    ujra = true;
+                }
+                else if (valasz == "n")
+                {
+                    ujra = false;
+                }
+                else
+                {
+                    Console.WriteLine("Hibás válasz, kérlek írd be újra!");
+                }
+            } while (ujra);
+        }
+
+        static void Display()
+        {
+        }
+
+        static void Winconditions()
+        {
         }
     }
 }
