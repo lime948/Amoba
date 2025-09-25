0d50608ac39821a59236c0c0e6765d493c9a79ec
OresZeteny <144881224+lime948@users.noreply.github.com>
2025-09-25 11:27:21 +0200

    Add files via upload

---
+++ Amoba/Amoba/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amoba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void GameLoop()
+        {
+            Console.WriteLine("=== AMŐBA ===");
+            bool ujra = false;
+            do
+            {
+                Console.WriteLine("Szeretnél újra játszani? (i/n)");
+                string valasz = Console.ReadLine().ToLower();
+                if (valasz == "i")
+                {
+                    ujra = true;
+                }
+                else if (valasz == "n")
+                {
+                    ujra = false;
+                }
+                else
+                {
+                    Console.WriteLine("Hibás válasz, kérlek írd be újra!");
+                }
+            } while (ujra);
+        }
+
+        static void Display()
+        {
+        }
+
+        static void Winconditions()
+        {
+        }
+    }
+}
