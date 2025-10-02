ee76be0de3e615597df52c1ed6d26b47d9967577
AteszXD <keria@kkszki.hu>
2025-10-02 12:04:10 +0200

    Felesleges "using" sorok törlése (futásidő csökkentése érdekében)

--- Amoba/Program.cs
+++ Amoba/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Media;
-using System.Security.Cryptography;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Amoba
 {
